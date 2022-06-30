95d45a3cbc20cccedca44770b2a8c076290732cc
Stricker2 <101269390+Stricker2@users.noreply.github.com>
2022-06-30 10:15:45 -0400

    add inquirer

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
-const profileDataArgs = process.argv.slice(2);
-const [name, github] = profileDataArgs;
+const inquirer = require('inquirer');
+console.log(inquirer);
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js');
+// const pageHTML = generatePage(name, github);
 
-fs.writeFile('index.html', generatePage(name, github), err => {
-    if (err) throw new Error(err);
+// fs.writeFile('index.html', pageHTML, err => {
+//     if (err) throw err;
 
-    console.log('Portfolio complete! Check out index.html to see the output!');
-});
+//     console.log('Portfolio complete! Check out index.html to see the output!');
+// });
+
